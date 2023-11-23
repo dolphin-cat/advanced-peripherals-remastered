65999c4bcdac703e65d96b502ac56f53f8487fed
bmartin127 <minecraft.bradley.martin@gmail.com>
2023-11-23 11:54:31 -0700

    pocket computer upgrade support fixed recipes hopefully item drops/loot tables

---
+++ src/main/java/bmartin127/advancedperipheralsremastered/APCreativeTab.java
@@ -0,0 +1,14 @@
+package bmartin127.advancedperipheralsremastered;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+import static bmartin127.advancedperipheralsremastered.Advancedperipherals.MODID;
+
+
+public class APCreativeTab {
+
+
+
+}
